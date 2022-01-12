0fbba41252cec48a18520c43f8bbacc6d6b9dc1e
kateowens12 <kate.owens@deque.com>
2022-01-12 17:21:50 -0500

    ok i think i am calling it a day now

--- Plantiverse/Views/AddPlant/PotSizeStepper.swift
+++ Plantiverse/Views/AddPlant/PotSizeStepper.swift
@@ -11,30 +11,32 @@ struct PotSizeStepper: View {
     @State var potSize: Int = 0
     
     var body: some View {
-        HStack {
-            Spacer()
-            Text("Pot Size")
-                .font(.body)
-                .padding()
-            Spacer()
-            VStack(alignment: .leading) {
-                Text("\($potSize.wrappedValue) inch")
+        VStack(alignment: .leading) {
+            HStack {
+                Spacer()
+                Text("Pot Size")
                     .font(.body)
-                    .padding(.bottom, 4)
-                HStack(alignment: .center) {
-                    IncrementButton()
-                    DecrementButton()
+                    .padding()
+                Spacer()
+                VStack {
+                    Text("\($potSize.wrappedValue) inch")
+                        .font(.body)
+                        .padding(.bottom, 4)
+                    HStack(alignment: .center) {
+                        IncrementButton()
+                        DecrementButton()
+                    }
                 }
+                Spacer()
             }
-            Spacer()
         }//.accessibilityElement(children: .combine)
-//// why am i forgetting all teh stepper things ahhh
-        /// add stepper a11y representation here once brain is less meh
-            .accessibilityRepresentation {
-                Stepper(value: $potSize, in: 0...1000) {
-                    Text("Pot size")
-                }.accessibilityValue(Text("\(potSize) inch"))
-            }
+        //// why am i forgetting all teh stepper things ahhh
+                /// add stepper a11y representation here once brain is less meh
+                    .accessibilityRepresentation {
+                        Stepper(value: $potSize, in: 0...1000) {
+                            Text("Pot size")
+                        }.accessibilityValue(Text("\(potSize) inch"))
+                }
         }
 }
 
